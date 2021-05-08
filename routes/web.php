b6ce0594a1c2be0834952f4b1740f9db5336be15
Đặng Thế Anh <dangtheanh16@gmail.com>
2021-05-08 14:03:25 +0700

    :sparkles: Update composer

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::group(['prefix' => 'auth'], function () {
     BackendRoutes::auth();
 });
 
-FrontendRoutes::web();
+//FrontendRoutes::web();
--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tadcms\Providers;
+namespace Tadcms\System\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
